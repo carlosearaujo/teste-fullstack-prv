b9ed3e203f5ecd5a75b9f5bceb5eab2228ff6b70
carlos.araujo <carlos.araujo@seduc.ce.gov.br>
2022-12-19 19:01:56 -0300

    Commit inicial - backend

---
+++ backend/src/test/java/br/com/unimedfortaleza/plano/PlanoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unimedfortaleza.plano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
